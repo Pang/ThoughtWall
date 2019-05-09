9df3fa7665a0691d8e51852a8fa05c8e1c89bfa0
cPango <ryan.beckett@outlook.com>
2019-05-09 22:44:19 +0100

    navbar component created

--- ThoughtWall-SPA/src/app/app.module.ts
+++ ThoughtWall-SPA/src/app/app.module.ts
@@ -2,11 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ThreadComponent } from './home-page/thread/thread.component';
 import { SubmitPageComponent } from './submit-page/submit-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomePageComponent, pathMatch: 'full' },
+  { path: 'submit', component: SubmitPageComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,13 +21,18 @@ import { SubmitPageComponent } from './submit-page/submit-page.component';
     HomePageComponent,
     ThreadComponent,
     SubmitPageComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
