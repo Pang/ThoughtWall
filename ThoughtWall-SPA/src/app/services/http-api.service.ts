904e69fdf6eab0ea90a3be0e3f2ac9c47203005a
cPango <ryan.beckett@outlook.com>
2019-06-01 21:45:12 +0100

    refactoring/cleaning httpApi service

--- ThoughtWall-SPA/src/app/services/http-api.service.ts
+++ ThoughtWall-SPA/src/app/services/http-api.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
