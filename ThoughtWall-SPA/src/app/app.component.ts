06ebb9ef54bb6a2f5ca932463d971f8c72d8f8de
cPango <ryan.beckett@outlook.com>
2019-12-27 12:59:37 +0000

    added check for token expire

--- ThoughtWall-SPA/src/app/app.component.ts
+++ ThoughtWall-SPA/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private jwtHelper: JwtHelperService){}
   title = 'ThoughtWall-SPA';
+  helper = new JwtHelperService();
+
+  ngOnInit() {
+    console.log(this.jwtHelper.getTokenExpirationDate());
+    if (!this.helper.isTokenExpired()) {
+      localStorage.removeItem('token');
+    }
+  }
 }
