83028e26fd4e3b59c2f76f4558232ccc69a35793
cPango <Ryan.beckett@outlook.com>
2021-08-03 23:57:43 +0100

    datetime swap using Intl

--- tw-react-spa/src/components/Threads/ThreadCard.js
+++ tw-react-spa/src/components/Threads/ThreadCard.js
@@ -1,12 +1,12 @@
 import BgImg from '../../images/logo512.png';
 
 const ThreadCard = ({ thread }) => {
-
+    const timeStamp = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full', timeStyle: 'short' }).format(new Date(thread.timeStamp));
     return(
         <div className="cardContainer m-2">
             <div style={{ backgroundImage: `url("${BgImg}")` }} className="card mx-auto threadCard">
                 <h5 className="text-primary mb-0">{thread.title}</h5>
-                <h6 className="text-secondary threadDt">01/01/21, 16:00</h6>
+                <h6 className="text-secondary threadDt">{timeStamp}</h6>
                 <p className="text-light threadDesc">{thread.body.slice(0, 100)}{thread.body.length > 100 && '...'}</p>
                 <span className="userTag">{thread.username}</span>
             </div>
